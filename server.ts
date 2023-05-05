bfe0c3ac1621f51fe446eae3fe5632419025c005
Ivan Atias <ivan.d.atias@gmail.com>
2023-05-05 14:39:12 -0400

    fix: handle not websocket connection request

--- server.ts
+++ server.ts
@@ -6,6 +6,14 @@ const port = 8080
 
 const handler = (req: Request) => {
   const { pathname } = new URL(req.url)
+  const upgrade = req.headers.get('upgrade')
+
+  if (upgrade !== 'websocket') {
+    return new Response('Request is not trying to upgrade web socket', {
+      headers: new Headers({ 'content-type': 'text/plain' }),
+      status: 400,
+    })
+  }
 
   if (pathname === '/start-chat') return handleSocket(req)
 
